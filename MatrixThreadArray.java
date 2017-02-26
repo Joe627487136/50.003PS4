ccb1fdd8e0addcf1bc01153ea153d9fe5e12ddc5
Zhou Xuexuan <joe627487136@hotmail.com>
2017-02-26 12:25:14 +0800

    small changes

--- MatrixThreadArray.java
+++ MatrixThreadArray.java
@@ -9,41 +9,55 @@ import java.util.Arrays;
 
 
 public class MatrixThreadArray {
-    public static int[][] A = {{1,3},{1,2},{5,3}};
-    public static int[][] B = {{8,7,6}, {5,4,3}};
+    public static int[][] A = {{1, 3}, {1, 2}, {5, 3}};
+    public static int[][] B = {{8, 7, 6}, {5, 4, 3}};
     public static int[][] C = new int[A.length][B[0].length];
-    public static DimensinalWorkingThread[][] Threads = new DimensinalWorkingThread[A.length][B[0].length];
+    public static void main(String[] args) throws Exception {
+        long startTime = System.currentTimeMillis();
+        int numberOfThreads = 2;
+        Thread[] threads = new Thread[numberOfThreads];
 
+        for (int i = 0; i < numberOfThreads; i++) {
+            threads[i] = new MatrixMultiThread(A,B,C,i*A.length/numberOfThreads,(i+1)*A.length/numberOfThreads);
+            threads[i].start();
+        }
 
-    public static void main(String[] args) {
-        for (int i = 0; i < A.length; i++) {
-            for (int j = 0; j < B[0].length; j++) {
-                Threads[i][j] = new DimensinalWorkingThread(i, j, A, B, C);
-                Threads[i][j].start();
+        try {
+            for (int i = 0; i < numberOfThreads; i++) {
+                threads[i].join();
+            }
+            long duration = System.currentTimeMillis() - startTime;
+            System.out.print("Time flied: " + duration+"\n");
+            System.out.println(Arrays.deepToString(C));
+        }
+        catch (InterruptedException e) {
+            System.out.println("A thread didn't finish!");
+        }
+    }
+
+    static class MatrixMultiThread extends Thread {
+        private int[][] A;
+        private int[][] B;
+        private int[][] C;
+        int lower;
+        int upper;
+
+        public MatrixMultiThread(int[][] A, int[][] B, int[][] C, int lower, int upper) {
+            this.A = A;
+            this.B = B;
+            this.C = C;
+            this.lower = lower;
+            this.upper = upper;
+        }
+
+        public void run() {
+            for (int i = lower; i < upper; i++) {
+                for (int j = 0; j < C[i].length; j++) {
+                    for (int k = 0; k < B.length; k++) {
+                        C[i][j] += A[i][k] * B[k][j];
+                    }
+                }
             }
         }
-        System.out.println((Arrays.deepToString(C)));
-    }
-}
-
-
-class DimensinalWorkingThread extends Thread{
-    private int row;
-    private int col;
-    private int [][] A;
-    private int [][] B;
-    private int [][] C;
-
-    public DimensinalWorkingThread(int row, int col, int[][] A,
-                        int[][] B, int[][] C) {
-        this.row = row;
-        this.col = col;
-        this.A = A;
-        this.B = B;
-        this.C = C;
-    }
-
-    public void run() {
-        C[row][col] = (A[row][0] * B[0][col])+ (A[row][1]*B[1][col]) ;
     }
 }
--- ThreadArrayExample.java
+++ ThreadArrayExample.java
@@ -6,20 +6,20 @@ public class ThreadArrayExample {
     	int numberOfThreads = 1;
     	Summer[] summers = new Summer[numberOfThreads];
     	Thread[] threads = new Thread[numberOfThreads];
-    	
+
     	for (int i = 0; i < numberOfThreads; i++) {
     		summers[i] = new Summer (array, i*array.length/numberOfThreads, ((i+1)*array.length/numberOfThreads));
     		threads[i] = new Thread(summers[i]);
     		threads[i].start();
     	}
-		
+
 		try {
 			int sum = 0;
 	    	for (int i = 0; i < numberOfThreads; i++) {
 	    		threads[i].join();
 	    		sum += summers[i].getSum();
 	    	}
-			
+
 			System.out.println("The sum is: " + sum);
 		}
 		catch (InterruptedException e) {
